3ee144c32b0206112d64370e30f612abbc0872e2
Susana Tavares <susana15tavares@hotmail.com>
2025-11-12 23:56:31 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Susana Tavares</h1>
+    </div>
+  );
+}
+
+export default App;
