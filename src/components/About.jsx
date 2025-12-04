0649960546e12141b193e1d0e82febc706da8668
Susana Tavares <susana15tavares@hotmail.com>
2025-12-04 23:35:59 +0000

    Profile photo added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,34 @@
 import "./About.css";
 import { motion } from "framer-motion";
+import profile from "../assets/susana.jpg";
 
 export default function About() {
   return (
     <section className="about" id="about">
-      <motion.div
-        initial={{ opacity: 0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 1 }}
-        className="about-content"
-      >
-        <h2>About</h2>
+      <div className="about-container">
+        
+        <motion.div
+          initial={{ opacity: 0, x: -40 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="about-image"
+        >
+          <img src={profile} alt="Profile" />
+        </motion.div>
 
+        <motion.div
+          initial={{ opacity: 0, x: 40 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="about-text"
+        >
+          <h2>About Me</h2>
           <p>🎓 3rd-year Software Engineering student @ Polytechnic University of Viseu  </p>
           <p>💻 Interested in Full Stack Development and Software Engineering</p>  
           <p>✨ I enjoy learning new technologies, solving problems and working on projects that make a difference.</p>
-        
-      </motion.div>
+        </motion.div>
+
+      </div>
     </section>
   );
 }
